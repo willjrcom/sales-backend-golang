251400a5a5642e9066745c48f9dc450a2a3bf82f
willjrcom <williamjunior67@gmail.com>
2023-09-12 15:50:24 -0300

    feat: category handler added

--- internal/infra/handler/product.go
+++ internal/infra/handler/product.go
@@ -1,10 +1,14 @@
 package handlerimpl
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/google/uuid"
 	"github.com/willjrcom/sales-backend-go/bootstrap/handler"
+	entitydto "github.com/willjrcom/sales-backend-go/internal/infra/dto/entity"
+	filterdto "github.com/willjrcom/sales-backend-go/internal/infra/dto/filter"
 	productdto "github.com/willjrcom/sales-backend-go/internal/infra/dto/product"
 	productusecases "github.com/willjrcom/sales-backend-go/internal/usecases/product"
 	jsonpkg "github.com/willjrcom/sales-backend-go/pkg/json"
@@ -26,7 +30,7 @@ func NewHandlerProduct(productService *productusecases.Service) *handler.Handler
 		c.Put("/update/{id}", h.handlerUpdateProduct)
 		c.Delete("/delete/{id}", h.handlerDeleteProduct)
 		c.Get("/{id}", h.handlerGetProduct)
-		c.Get("/all", h.handlerGetAllProducts)
+		c.Post("/all", h.handlerGetAllProducts)
 	})
 
 	return handler.NewHandler("/product", c)
@@ -48,21 +52,80 @@ func (h *handlerProductImpl) handlerRegisterProduct(w http.ResponseWriter, r *ht
 }
 
 func (h *handlerProductImpl) handlerUpdateProduct(w http.ResponseWriter, r *http.Request) {
-	//ctx := r.Context()
-	w.Write([]byte("update product"))
+	ctx := r.Context()
+
+	id := chi.URLParam(r, "id")
+	dtoId := &entitydto.IdRequest{ID: uuid.MustParse(id)}
+
+	product := &productdto.UpdateProductInput{}
+	jsonpkg.ParseBody(r, product)
+
+	err := h.ps.UpdateProduct(ctx, dtoId, product)
+
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Write([]byte("new product:"))
 }
 
 func (h *handlerProductImpl) handlerDeleteProduct(w http.ResponseWriter, r *http.Request) {
-	//ctx := r.Context()
+	ctx := r.Context()
+
+	id := chi.URLParam(r, "id")
+	dtoId := &entitydto.IdRequest{ID: uuid.MustParse(id)}
+
+	err := h.ps.DeleteProductById(ctx, dtoId)
+
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
 	w.Write([]byte("delete product"))
 }
 
 func (h *handlerProductImpl) handlerGetProduct(w http.ResponseWriter, r *http.Request) {
-	//ctx := r.Context()
-	w.Write([]byte("get product"))
+	ctx := r.Context()
+
+	id := chi.URLParam(r, "id")
+	dtoId := &entitydto.IdRequest{ID: uuid.MustParse(id)}
+
+	product, err := h.ps.GetProductById(ctx, dtoId)
+
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	text, err := json.Marshal(product)
+
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Write(text)
 }
 
 func (h *handlerProductImpl) handlerGetAllProducts(w http.ResponseWriter, r *http.Request) {
-	//ctx := r.Context()
-	w.Write([]byte("get all product"))
+	ctx := r.Context()
+	filter := &filterdto.Category{}
+	jsonpkg.ParseBody(r, filter)
+
+	categories, err := h.ps.GetAllProductsByCategory(ctx, filter)
+
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	text, err := json.Marshal(categories)
+
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Write(text)
 }
