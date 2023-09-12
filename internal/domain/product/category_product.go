beb16c57985555f4ba7a35f13e9d7750cf70770c
willjrcom <williamjunior67@gmail.com>
2023-09-12 15:51:33 -0300

    fix: change relations bun

--- internal/domain/product/category_product.go
+++ internal/domain/product/category_product.go
@@ -1,12 +1,14 @@
 package productentity
 
 import (
+	"github.com/uptrace/bun"
 	"github.com/willjrcom/sales-backend-go/internal/domain/entity"
 )
 
 type CategoryProduct struct {
 	entity.Entity
-	Name     string     `bun:"name"`
-	Sizes    []string   `bun:"sizes"`
-	Products []*Product `bun:"products,rel:has-many,join:id=id"`
+	bun.BaseModel `bun:"table:category_products"`
+	Name          string     `bun:"name"`
+	Sizes         []string   `bun:"sizes"`
+	Products      []*Product `bun:"rel:has-many,join:id=category_id"`
 }
--- internal/domain/product/product.go
+++ internal/domain/product/product.go
@@ -2,18 +2,20 @@ package productentity
 
 import (
 	"github.com/google/uuid"
+	"github.com/uptrace/bun"
 	"github.com/willjrcom/sales-backend-go/internal/domain/entity"
 )
 
 type Product struct {
 	entity.Entity
-	Code        string           `bun:"code"`
-	Name        string           `bun:"name"`
-	Description string           `bun:"description"`
-	Size        string           `bun:"size"`
-	Price       float64          `bun:"price"`
-	Cost        float64          `bun:"cost"`
-	CategoryID  uuid.UUID        `bun:"category_id,notnull"`
-	Category    *CategoryProduct `bun:"rel:has-one"`
-	IsAvailable bool             `bun:"is_available"`
+	bun.BaseModel `bun:"table:products"`
+	Code          string           `bun:"code"`
+	Name          string           `bun:"name"`
+	Description   string           `bun:"description"`
+	Size          string           `bun:"size"`
+	Price         float64          `bun:"price"`
+	Cost          float64          `bun:"cost"`
+	CategoryID    uuid.UUID        `bun:"column:category_id,type:uuid,notnull"`
+	Category      *CategoryProduct `bun:"rel:belongs-to"`
+	IsAvailable   bool             `bun:"is_available"`
 }
