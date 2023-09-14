1a8da7bc9d65e33ea8872294083949eb2a100a63
willjrcom <williamjunior67@gmail.com>
2023-09-13 23:09:14 -0300

    feat: clientRepository added

--- internal/infra/repository/postgres/client/client.go
+++ internal/infra/repository/postgres/client/client.go
@@ -1,37 +1,102 @@
 package clientrepositorylocal
 
 import (
+	"sync"
+
+	"github.com/uptrace/bun"
 	cliententity "github.com/willjrcom/sales-backend-go/internal/domain/client"
 	"golang.org/x/net/context"
 )
 
-type ProductRepositoryBun struct {
+type ClientRepositoryBun struct {
+	mu sync.Mutex
+	db *bun.DB
 }
 
-func NewProductRepositoryLocal() *ProductRepositoryBun {
-	return &ProductRepositoryBun{}
+func NewClientRepositoryLocal(db *bun.DB) *ClientRepositoryBun {
+	return &ClientRepositoryBun{db: db}
 }
 
-func (r *ProductRepositoryBun) RegisterClient(ctx context.Context, c *cliententity.Client) error {
+func (r *ClientRepositoryBun) RegisterClient(ctx context.Context, c *cliententity.Client) error {
+	r.mu.Lock()
+	_, err := r.db.NewInsert().Model(c).Exec(ctx)
+	r.mu.Unlock()
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (r *ProductRepositoryBun) UpdateClient(ctx context.Context, c *cliententity.Client) error {
+func (r *ClientRepositoryBun) UpdateClient(ctx context.Context, c *cliententity.Client) error {
+	r.mu.Lock()
+	_, err := r.db.NewUpdate().Model(c).Where("id = ?", c.ID).Exec(ctx)
+	r.mu.Unlock()
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (r *ProductRepositoryBun) DeleteClient(ctx context.Context, id string) error {
+func (r *ClientRepositoryBun) DeleteClient(ctx context.Context, id string) error {
+	r.mu.Lock()
+	_, err := r.db.NewDelete().Model(&cliententity.Client{}).Where("id = ?", id).Exec(ctx)
+	r.mu.Unlock()
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func (r *ProductRepositoryBun) GetClientById(ctx context.Context, id string) (*cliententity.Client, error) {
-	return nil, nil
+func (r *ClientRepositoryBun) GetClientById(ctx context.Context, id string) (*cliententity.Client, error) {
+	client := &cliententity.Client{}
+
+	r.mu.Lock()
+	err := r.db.NewSelect().Model(client).Where("client.id = ?", id).Relation("Address").Scan(ctx)
+	r.mu.Unlock()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
 
-func (r *ProductRepositoryBun) GetClientBy(ctx context.Context, c *cliententity.Client) (*cliententity.Client, error) {
-	return nil, nil
+func (r *ClientRepositoryBun) GetClientBy(ctx context.Context, c *cliententity.Client) ([]cliententity.Client, error) {
+	clients := []cliententity.Client{}
+
+	r.mu.Lock()
+	query := r.db.NewSelect().Model(&cliententity.Client{})
+
+	if c.Name != "" {
+		query.Where("client.code = ?", c.Name)
+	}
+
+	err := query.Relation("Address").Scan(ctx, &clients)
+	r.mu.Unlock()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return clients, nil
 }
 
-func (r *ProductRepositoryBun) GetAllClient(ctx context.Context) ([]cliententity.Client, error) {
-	return nil, nil
+func (r *ClientRepositoryBun) GetAllClient(ctx context.Context) ([]cliententity.Client, error) {
+	clients := []cliententity.Client{}
+	r.mu.Lock()
+	err := r.db.NewSelect().Model(&clients).Relation("Address").Scan(ctx)
+
+	r.mu.Unlock()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return clients, nil
 }
