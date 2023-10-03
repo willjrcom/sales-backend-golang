16313477fb83c3e8b2a8b95df3362b10af2bbbb9
willjrcom <williamjunior67@gmail.com>
2023-10-03 10:02:39 -0300

    feat: set state status added

--- internal/domain/order/order.go
+++ internal/domain/order/order.go
@@ -1,6 +1,8 @@
 package orderentity
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/uptrace/bun"
 	employeeentity "github.com/willjrcom/sales-backend-go/internal/domain/employee"
@@ -11,14 +13,56 @@ import (
 type Order struct {
 	entity.Entity
 	bun.BaseModel `bun:"table:orders"`
-	PaymentOrder
-	Name        string                   `bun:"name,notnull"`
-	OrderNumber int                      `bun:"order_number,notnull"`
-	Delivery    *DeliveryOrder           `bun:"rel:has-one,join:id=order_id"`
-	TableOrder  *TableOrder              `bun:"rel:has-one,join:id=order_id"`
-	AttendantID uuid.UUID                `bun:"column:attendant_id,type:uuid,notnull"`
-	Attendant   *employeeentity.Employee `bun:"rel:belongs-to"`
-	Status      StatusOrder              `bun:"status,type:enum,notnull"`
-	Observation string                   `bun:"observation"`
-	GroupItems  []itementity.GroupItem   `bun:"rel:has-many,join:id=order_id"`
+	Payment       *PaymentOrder
+	Name          string                   `bun:"name"`
+	Observation   string                   `bun:"observation"`
+	OrderNumber   int                      `bun:"order_number,notnull"`
+	Status        StatusOrder              `bun:"status,notnull"`
+	Delivery      *DeliveryOrder           `bun:"rel:has-one,join:id=order_id"`
+	Table         *TableOrder              `bun:"rel:has-one,join:id=order_id"`
+	AttendantID   uuid.UUID                `bun:"column:attendant_id,type:uuid"`
+	Attendant     *employeeentity.Employee `bun:"rel:belongs-to"`
+	Groups        []itementity.GroupItem   `bun:"rel:has-many,join:id=order_id"`
+	LaunchedAt    *time.Time               `bun:"launch"`
+}
+
+func NewDefaultOrder() *Order {
+	return &Order{
+		Entity: entity.NewEntity(),
+		Status: OrderStatusStaging,
+	}
+}
+
+func (o *Order) StagingOrder() {
+	o.Status = OrderStatusStaging
+}
+
+func (o *Order) LaunchOrder() {
+	o.Status = OrderStatusPending
+	*o.LaunchedAt = time.Now()
+}
+
+func (o *Order) ReadyOrder() {
+	o.Status = OrderStatusFinished
+}
+
+func (o *Order) ShipOrder() {
+	o.Status = OrderStatusShipped
+	*(*o.Delivery).Pickup = time.Now()
+}
+
+func (o *Order) FinishOrder() {
+	o.Status = OrderStatusFinished
+
+	if o.Delivery != nil {
+		*(*o.Delivery).Delivered = time.Now()
+	}
+}
+
+func (o *Order) CancelOrder() {
+	o.Status = OrderStatusCanceled
+}
+
+func (o *Order) ArchiveOrder() {
+	o.Status = OrderStatusArchived
 }
