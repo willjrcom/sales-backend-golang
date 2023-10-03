569c033c58d61a430d503ca30ae47c867551e1d0
willjrcom <williamjunior67@gmail.com>
2023-10-03 10:01:27 -0300

    feat: address added

--- internal/domain/order/delivery_order.go
+++ internal/domain/order/delivery_order.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/uptrace/bun"
+	addressentity "github.com/willjrcom/sales-backend-go/internal/domain/address"
 	cliententity "github.com/willjrcom/sales-backend-go/internal/domain/client"
 	employeeentity "github.com/willjrcom/sales-backend-go/internal/domain/employee"
 	"github.com/willjrcom/sales-backend-go/internal/domain/entity"
@@ -17,10 +18,12 @@ type DeliveryOrder struct {
 	Pickup        *time.Time               `bun:"pickup"`
 	Delivered     *time.Time               `bun:"delivered"`
 	IsCompleted   bool                     `bun:"is_completed"`
-	DeliveryTax   float64                  `bun:"delivery_tax"`
+	DeliveryTax   *float64                 `bun:"delivery_tax"`
 	ClientID      uuid.UUID                `bun:"column:client_id,type:uuid,notnull"`
 	Client        *cliententity.Client     `bun:"rel:belongs-to"`
-	DriverID      uuid.UUID                `bun:"column:driver_id,type:uuid,notnull"`
+	AddressID     uuid.UUID                `bun:"column:address_id,type:uuid,notnull"`
+	Address       *addressentity.Address   `bun:"rel:belongs-to"`
+	DriverID      *uuid.UUID               `bun:"column:driver_id,type:uuid,notnull"`
 	Driver        *employeeentity.Employee `bun:"rel:belongs-to"`
 }
 
