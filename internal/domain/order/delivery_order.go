8d3ede829789a16efc3cce9bff929814e6def35a
willjrcom <williamjunior67@gmail.com>
2023-11-10 21:47:01 -0300

    refactor: modelBase removed

--- internal/domain/order/delivery_order.go
+++ internal/domain/order/delivery_order.go
@@ -14,22 +14,21 @@ import (
 type DeliveryOrder struct {
 	entity.Entity
 	bun.BaseModel `bun:"table:delivery_orders"`
-	ModelBase
+	OrderID       uuid.UUID                `bun:"column:order_id,type:uuid,notnull"`
+	Pickup        *time.Time               `bun:"pickup"`
+	Delivered     *time.Time               `bun:"delivered"`
+	IsCompleted   bool                     `bun:"is_completed"`
+	DeliveryTax   *float64                 `bun:"delivery_tax"`
+	Status        StatusDeliveryOrder      `bun:"status"`
+	ClientID      uuid.UUID                `bun:"column:client_id,type:uuid,notnull"`
+	Client        *cliententity.Client     `bun:"rel:belongs-to"`
+	AddressID     uuid.UUID                `bun:"column:address_id,type:uuid,notnull"`
+	Address       *addressentity.Address   `bun:"rel:belongs-to"`
+	DriverID      *uuid.UUID               `bun:"column:driver_id,type:uuid"`
+	Driver        *employeeentity.Employee `bun:"rel:belongs-to"`
 }
 
 type ModelBase struct {
-	OrderID     uuid.UUID                `bun:"column:order_id,type:uuid,notnull"`
-	Pickup      *time.Time               `bun:"pickup"`
-	Delivered   *time.Time               `bun:"delivered"`
-	IsCompleted bool                     `bun:"is_completed"`
-	DeliveryTax *float64                 `bun:"delivery_tax"`
-	Status      StatusDeliveryOrder      `bun:"status"`
-	ClientID    uuid.UUID                `bun:"column:client_id,type:uuid,notnull"`
-	Client      *cliententity.Client     `bun:"rel:belongs-to"`
-	AddressID   uuid.UUID                `bun:"column:address_id,type:uuid,notnull"`
-	Address     *addressentity.Address   `bun:"rel:belongs-to"`
-	DriverID    *uuid.UUID               `bun:"column:driver_id,type:uuid"`
-	Driver      *employeeentity.Employee `bun:"rel:belongs-to"`
 }
 
 func (d *DeliveryOrder) LaunchDelivery(driver *employeeentity.Employee) {
