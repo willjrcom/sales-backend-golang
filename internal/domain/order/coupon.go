2b0adbc37a8079112999bfe66e606c5a89b2ecd5
willjrcom <williamjunior67@gmail.com>
2024-02-18 20:25:02 -0300

    feat: coupon added to domain

---
+++ internal/domain/order/coupon.go
@@ -0,0 +1,44 @@
+package orderentity
+
+import (
+	"errors"
+	"time"
+
+	"github.com/uptrace/bun"
+	"github.com/willjrcom/sales-backend-go/internal/domain/entity"
+)
+
+var (
+	ErrDiscountMustBePositive = errors.New("discount must be positive")
+	ErrStartAndEndAtRequired  = errors.New("start_at and end_at are required")
+	ErrStartAtAfterEndAt      = errors.New("start_at must be before end_at")
+)
+
+type Coupon struct {
+	entity.Entity
+	bun.BaseModel `bun:"table:coupons"`
+	CouponCommonAttributes
+}
+
+type CouponCommonAttributes struct {
+	Discount float64    `bun:"discount" json:"discount"`
+	Min      float64    `bun:"min" json:"min"`
+	StartAt  *time.Time `bun:"start_at" json:"start_at"`
+	EndAt    *time.Time `bun:"end_at" json:"end_at"`
+}
+
+func NewCoupon(couponCommonAttributes CouponCommonAttributes) (*Coupon, error) {
+	if couponCommonAttributes.Discount <= 0 {
+		return nil, ErrDiscountMustBePositive
+	}
+
+	if couponCommonAttributes.StartAt == nil || couponCommonAttributes.EndAt == nil {
+		return nil, ErrStartAndEndAtRequired
+	}
+
+	if (*couponCommonAttributes.StartAt).After(*couponCommonAttributes.EndAt) {
+		return nil, ErrStartAtAfterEndAt
+	}
+
+	return &Coupon{Entity: entity.NewEntity(), CouponCommonAttributes: couponCommonAttributes}, nil
+}
