b47acfe7d1904809e0b36b01d99206d83de3d002
willjrcom <williamjunior67@gmail.com>
2024-03-12 17:23:20 -0300

    feat: item trigger to group item added

--- internal/usecases/item/item.go
+++ internal/usecases/item/item.go
@@ -97,43 +97,83 @@ func (s *Service) AddItemOrder(ctx context.Context, dto *itemdto.AddItemOrderInp
 }
 
 func (s *Service) StartItem(ctx context.Context, dto *entitydto.IdRequest) (err error) {
-	groupItem, err := s.ri.GetItemById(ctx, dto.ID.String())
+	item, err := s.ri.GetItemById(ctx, dto.ID.String())
 
 	if err != nil {
 		return err
 	}
 
-	if err = groupItem.StartItem(); err != nil {
+	if err = item.StartItem(); err != nil {
 		return err
 	}
 
-	return s.ri.UpdateItem(ctx, groupItem)
+	groupItem, err := s.rgi.GetGroupByID(ctx, item.GroupItemID.String(), false)
+
+	if err != nil {
+		return err
+	}
+
+	if groupItem.Status == groupitementity.StatusGroupPending {
+		groupItem.StartGroupItem()
+	}
+
+	if err = s.ri.UpdateItem(ctx, item); err != nil {
+		return err
+	}
+
+	return s.rgi.UpdateGroupItem(ctx, groupItem)
 }
 
 func (s *Service) ReadyItem(ctx context.Context, dto *entitydto.IdRequest) (err error) {
-	groupItem, err := s.ri.GetItemById(ctx, dto.ID.String())
+	item, err := s.ri.GetItemById(ctx, dto.ID.String())
 
 	if err != nil {
 		return err
 	}
 
-	if err = groupItem.ReadyItem(); err != nil {
+	if err = item.ReadyItem(); err != nil {
 		return err
 	}
 
-	return s.ri.UpdateItem(ctx, groupItem)
+	groupItem, err := s.rgi.GetGroupByID(ctx, item.GroupItemID.String(), false)
+
+	if err != nil {
+		return err
+	}
+
+	if err = s.ri.UpdateItem(ctx, item); err != nil {
+		return err
+	}
+
+	isAllItemsReady := true
+	for i := range groupItem.Items {
+		if groupItem.Items[i].Status != itementity.StatusItemReady {
+			isAllItemsReady = false
+			break
+		}
+	}
+
+	if !isAllItemsReady {
+		return nil
+	}
+
+	if err = groupItem.ReadyGroupItem(); err != nil {
+		return err
+	}
+
+	return s.rgi.UpdateGroupItem(ctx, groupItem)
 }
 
 func (s *Service) CancelItem(ctx context.Context, dto *entitydto.IdRequest) (err error) {
-	groupItem, err := s.ri.GetItemById(ctx, dto.ID.String())
+	item, err := s.ri.GetItemById(ctx, dto.ID.String())
 
 	if err != nil {
 		return err
 	}
 
-	groupItem.CancelItem()
+	item.CancelItem()
 
-	return s.ri.UpdateItem(ctx, groupItem)
+	return s.ri.UpdateItem(ctx, item)
 }
 
 func (s *Service) DeleteItemOrder(ctx context.Context, dto *entitydto.IdRequest) (err error) {
