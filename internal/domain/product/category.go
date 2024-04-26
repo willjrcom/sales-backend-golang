87413d4e178f39123dcdd983ee49275a6de4ba7a
willjrcom <williamjunior67@gmail.com>
2024-04-26 10:05:19 -0300

    fix: removable_ingredients json

--- internal/domain/product/category.go
+++ internal/domain/product/category.go
@@ -15,7 +15,7 @@ type CategoryCommonAttributes struct {
 	Name                 string        `bun:"name,unique,notnull" json:"name"`
 	ImagePath            string        `bun:"image_path" json:"image_path"`
 	NeedPrint            bool          `bun:"need_print,notnull" json:"need_print"`
-	RemovableIngredients []string      `bun:"removable_ingredients,type:jsonb" json:"removable_items,omitempty"`
+	RemovableIngredients []string      `bun:"removable_ingredients,type:jsonb" json:"removable_ingredients,omitempty"`
 	Sizes                []Size        `bun:"rel:has-many,join:id=category_id" json:"sizes,omitempty"`
 	Quantities           []Quantity    `bun:"rel:has-many,join:id=category_id" json:"quantities,omitempty"`
 	Products             []Product     `bun:"rel:has-many,join:id=category_id" json:"products,omitempty"`
