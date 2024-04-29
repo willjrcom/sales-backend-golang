6ff487f6d3e8414e65e6506354b7c6c4953c39a7
willjrcom <williamjunior67@gmail.com>
2024-04-29 16:36:22 -0300

    feat: get last process rule

--- internal/infra/repository/postgres/process_rule/process.go
+++ internal/infra/repository/postgres/process_rule/process.go
@@ -2,8 +2,11 @@ package processrulerepositorybun
 
 import (
 	"context"
+	"fmt"
+	"strings"
 	"sync"
 
+	"github.com/google/uuid"
 	"github.com/uptrace/bun"
 	"github.com/willjrcom/sales-backend-go/bootstrap/database"
 	productentity "github.com/willjrcom/sales-backend-go/internal/domain/product"
@@ -78,4 +81,85 @@ func (r *ProcessRuleCategoryRepositoryBun) GetProcessRuleById(ctx context.Contex
 	}
 
 	return processRule, nil
+}
+
+func (r *ProcessRuleCategoryRepositoryBun) GetFirstProcessRuleByCategoryId(ctx context.Context, id string) (*productentity.ProcessRule, error) {
+	processRule := &productentity.ProcessRule{}
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if err := database.ChangeSchema(ctx, r.db); err != nil {
+		return nil, err
+	}
+
+	if err := r.db.NewSelect().Model(processRule).Where("category_id = ? and order = 1", id).Scan(ctx); err != nil {
+		return nil, err
+	}
+
+	return processRule, nil
+}
+
+func (r *ProcessRuleCategoryRepositoryBun) GetMapProcessRulesByFirstOrder(ctx context.Context) (map[uuid.UUID]uuid.UUID, error) {
+	processRules := []productentity.ProcessRule{}
+	mapProcesses := map[uuid.UUID]uuid.UUID{}
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if err := database.ChangeSchema(ctx, r.db); err != nil {
+		return nil, err
+	}
+
+	if err := r.db.NewSelect().Model(&processRules).Where("order = 1").Scan(ctx); err != nil {
+		return nil, err
+	}
+
+	for _, processRule := range processRules {
+		mapProcesses[processRule.CategoryID] = processRule.ID
+	}
+
+	return mapProcesses, nil
+}
+
+func (r *ProcessRuleCategoryRepositoryBun) GetMapProcessRulesByLastOrder(ctx context.Context) (map[uuid.UUID]uuid.UUID, error) {
+	processRulesSubquery := []productentity.ProcessRule{}
+	processRules := []productentity.ProcessRule{}
+	mapProcesses := map[uuid.UUID]uuid.UUID{}
+
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if err := database.ChangeSchema(ctx, r.db); err != nil {
+		return nil, err
+	}
+
+	// Subconsulta para obter o máximo order para cada category_id
+	err := r.db.NewSelect().Model(&processRulesSubquery).ColumnExpr("category_id, MAX(\"order\") AS order").
+		Group("category_id").Scan(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var pairs []string
+	for _, result := range processRulesSubquery {
+		pair := fmt.Sprintf("('%s', %d)", result.CategoryID, result.Order)
+		pairs = append(pairs, pair)
+	}
+
+	// Consulta principal que compara category_id e order com os valores da subconsulta
+	err = r.db.NewSelect().
+		Model(&processRules).
+		Where(fmt.Sprintf("(category_id, \"order\") IN (%s)", strings.Join(pairs, ","))).
+		Scan(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	for _, processRule := range processRulesSubquery {
+		mapProcesses[processRule.CategoryID] = processRule.ID
+	}
+
+	return mapProcesses, nil
 }
