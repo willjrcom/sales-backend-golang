33bd208bf8a2503f6d449ebff9d1fc58f24ad767
willjrcom <williamjunior67@gmail.com>
2024-05-13 14:02:20 -0300

    refactor: accessToken to access_token

--- internal/infra/dto/user/token_output.go
+++ internal/infra/dto/user/token_output.go
@@ -3,6 +3,6 @@ package userdto
 import companyentity "github.com/willjrcom/sales-backend-go/internal/domain/company"
 
 type TokenAndSchemasOutput struct {
-	AccessToken string                           `json:"accessToken"`
+	AccessToken string                           `json:"access_token"`
 	Companies   []companyentity.CompanyWithUsers `json:"companies"`
 }
