fa02b0c2e318446dd71abeae904959309cd68a64
willjrcom <williamjunior67@gmail.com>
2026-01-24 15:57:55 -0300

    fix: migration

--- bootstrap/database/migrations/20260123-add_product_info_to_stock_alerts.sql
+++ bootstrap/database/migrations/20260123-add_product_info_to_stock_alerts.sql
@@ -1,5 +1,5 @@
 -- Add product_name and product_sku to stock_alerts table
 
 -- Up
-ALTER TABLE stock_alerts ADD COLUMN product_name TEXT;
-ALTER TABLE stock_alerts ADD COLUMN product_code TEXT;
+ALTER TABLE stock_alerts ADD COLUMN IF NOT EXISTS product_name TEXT;
+ALTER TABLE stock_alerts ADD COLUMN IF NOT EXISTS product_code TEXT;
