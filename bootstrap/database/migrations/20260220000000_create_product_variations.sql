5e043ef3ec9ea8a212b90eaac1ea17cd7d0bc4b0
willjrcom <williamjunior67@gmail.com>
2026-02-26 13:32:48 -0300

    fix: migration

--- bootstrap/database/migrations/20260220000000_create_product_variations.sql
+++ bootstrap/database/migrations/20260220000000_create_product_variations.sql
@@ -9,17 +9,10 @@ CREATE TABLE if not exists "product_variations" (
   "updated_at" timestamptz DEFAULT now(),
   "deleted_at" timestamptz,
   PRIMARY KEY ("id"),
-  FOREIGN KEY ("product_id") REFERENCES "products" ("id") ON UPDATE NO ACTION ON DELETE CASCADE,
-  FOREIGN KEY ("size_id") REFERENCES "sizes" ("id") ON UPDATE NO ACTION ON DELETE CASCADE
 );
 
--- Migrate existing data
-INSERT INTO "product_variations" ("product_id", "size_id", "price", "cost", "is_available", "created_at", "updated_at", "deleted_at")
-SELECT "id", "size_id", "price", "cost", "is_available", "created_at", "updated_at", "deleted_at"
-FROM "products";
-
 -- Drop columns from products
-ALTER TABLE "products" DROP COLUMN "price";
-ALTER TABLE "products" DROP COLUMN "cost";
-ALTER TABLE "products" DROP COLUMN "is_available";
-ALTER TABLE "products" DROP COLUMN "size_id";
+ALTER TABLE "products" DROP COLUMN IF EXISTS "price";
+ALTER TABLE "products" DROP COLUMN IF EXISTS "cost";
+ALTER TABLE "products" DROP COLUMN IF EXISTS "is_available";
+ALTER TABLE "products" DROP COLUMN IF EXISTS "size_id";
